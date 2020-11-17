47387a204ebce36358decc699d662246e8e0b4b4
necococo <piyohikoninjya@gmail.com>
2020-11-17 03:20:53 +0000

    navbar-default navvar-header=brand only

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,5 +1,5 @@
 <header>
-    <nav class="navbar navbar-inverse navbar-static-top">
+    <nav class="navbar navbar-default navbar-static-top">
         <div class="container-fluid">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -10,11 +10,13 @@
                 </button>
                 <!--タイトル-->
                 <a class="navbar-brand" href="/">NecoNeco</a>
+            </div>    
                 
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <!--検索バー-->
-                    <ul class="nav navbar-nav">
-                        @if(Auth::check())
+                    
+                     @if(Auth::check())
+                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                        <!--検索バー-->
                             <li>
                                 <form id="search-box" action="/microposts/search" method="GET">
                                   　<div id="search_field" class="input-group">
@@ -25,35 +27,34 @@
                                     </div>
                                 </form>
                             </li>
-                        @endif
-                    </ul>
-                    
-                    
-                    <ul class="nav navbar-nav navbar-right">
-                     
-                        <li><a href="/usage">Usage</a></li>
-                    @if (Auth::check())
-                        <li><a href="/">All Cats Photos</a></li> 
-                        <li>{!! link_to_route('microposts.all_map','All Photos Map') !!}</li>
-                        <li>{!! link_to_route('users.index','Other Users') !!}</li>
-                        <li>{!! link_to_route('microposts.create','New Post') !!}</li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li>{!! link_to_route('users.show', 'My_profile', ['id'=>Auth::id()]) !!}</li>
-                                <li role="separator" class="divider"></li>
-                                <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
-                            </ul>
-                        </li>
-                       
+                        </ul>
+
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="/usage">Usage</a></li>
+                            <li><a href="/">All Cats Photos</a></li> 
+                            <li>{!! link_to_route('microposts.all_map','All Photos Map') !!}</li>
+                            <li>{!! link_to_route('users.index','Other Users') !!}</li>
+                            <li>{!! link_to_route('microposts.create','New Post') !!}</li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li>{!! link_to_route('users.show', 'My_profile', ['id'=>Auth::id()]) !!}</li>
+                                    <li role="separator" class="divider"></li>
+                                    <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
+                                </ul>
+                            </li>
+                        </ul>
+                        </div>
                     @else
-                        <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
-                        <li>{!! link_to_route('login', 'Login') !!}</li>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="/usage">Usage</a></li>
+                            <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
+                            <li>{!! link_to_route('login', 'Login') !!}</li>
+                        </ul>
+                        </div>
                     @endif
                     
-                    </ul>
-                </div>
-            </div>
-        </div>
+        <!--</div>-->
     </nav>
 </header>
